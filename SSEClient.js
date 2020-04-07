9ae904b7b09778d2666d302b9b5d1e4c564c6565
xefiji <fxechappe@gmail.com>
2020-04-07 23:45:01 +0200

    Handle with creds / with token

--- SSEClient.js
+++ SSEClient.js
@@ -9,46 +9,63 @@
  * @param id
  * @constructor
  */
-function SSEClient(url, event, user, password, id) {
+function SSEClient(url, event) {
 
-	this.url = url;
-	this.event = event;
-	this.user = user;
-	this.password = password;
-	this.id = id;
+        this.url = url;
+        this.event = event;
 
-	// the handler function that will be called  to do something with the event
-	this.setCallback = function (callback) {
-		this.callback = callback;
-	};
+        // the handler function that will be called  to do something with the event
+        this.setCallback = function (callback) {
+            this.callback = callback;
+        };
 
-	//build a json with user's datas
-	this.buildLoginPayload = function () {
-		var data = {};
-		data.username = this.user;
-		data.password = this.password;
-		data.id = this.id;
-		return JSON.stringify(data);
-	};
+        //warning: data passes clearly in the request!
+        this.withCreds = function (user, password, id) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", this.url + "/creds_login", true);
+            xhr.withCredentials = true;
+            xhr.setRequestHeader('Content-type', 'application/json');
 
-	//logs in, then when its logged (hence cookie set), instanciate an EventStream object
-	this.run = function () {
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", this.url + "/login", true);
-		xhr.withCredentials = true;
-		xhr.setRequestHeader('Content-type', 'application/json');
-		xhr.send(this.buildLoginPayload());
+            var data = {};
+            data.username = user;
+            data.password = password;
+            data.id = id;
 
-		var url = this.url;
-		var eventType = this.event;
-		var callback = this.callback;
-		xhr.addEventListener("loadend", function (event) {
-			const evtSource = new EventSource(url + "/events?type=" + eventType, {withCredentials: true});
-			evtSource.onmessage = function (event) {
-				callback(event)
-			};
-		}, false);
-	};
-};
+            xhr.send(JSON.stringify(data));
+            var url = this.url;
+            var eventType = this.event;
+            var callback = this.callback;
+
+            var run = this.run;
+            var eventType = this.event;
+            var url = this.url;
+            var callback = this.callback;
+            xhr.addEventListener("loadend", function (event) {
+                run(url, eventType, callback);
+            }, false);
+        };
+
+        //use this if token was generated server side
+        this.withToken = function (token, id) {
+            //encode token
+            var data = {}
+            data.token = token;
+            data.id = id;
+            var encoded = btoa(JSON.stringify(data));
+            //set cookie
+            document.cookie = "sse_token=" + encoded;
+            //callrun
+            this.run(this.url, this.event, this.callback);
+        };
+
+        //after logged in (hence cookie set), instanciate an EventStream object
+        this.run = function (url, eventType, callback) {            
+            const evtSource = new EventSource(url + "/events?type=" + eventType, { withCredentials: true });        
+            evtSource.onmessage = function (event) {
+                callback(event)
+            };
+        };
+
+    };
 
 </script>
